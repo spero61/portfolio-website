36caebcfef80d596ef9e9220c24c806d18ccb8ca
spero61 <spero61@gmail.com>
2022-05-28 22:29:00 +0900

    Add Layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,29 @@
-import Sidebar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
 import './index.scss';
+import Sidebar from '../Sidebar';
 
 const Layout = () => (
-  <Sidebar />
+  <div className="App">
+    <Sidebar />
+    <div className="page">
+
+      <span className="tags top-tags top-tag-html">
+        &lt;html&gt;
+        <br />
+        <span className="top-body-tag">
+          &lt;body&gt;
+        </span>
+      </span>
+
+      <Outlet />
+
+      <span className="tags bottom-tags">
+        &lt;/body&gt;
+        <br />
+        <span className="bottom-tag-html">&lt;/html&gt;</span>
+      </span>
+    </div>
+  </div>
 );
 
 export default Layout;
