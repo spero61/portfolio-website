8113d6052afa313751bce1fd12efe9eeb9d92a33
spero61 <spero61@gmail.com>
2022-05-29 22:47:48 +0900

    Add AnimatedLetters component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,47 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
-const Home = () => (
-  <div className="container home-page">
-    <div className="text-zone">
-      <h1>
-        Hi,
-        <br />
-        I&#39;m
-        <img src={LogoTitle} alt="developer" />
-        ung
-        <br />
-        web developer
-      </h1>
-      <h2>Frontend Developer</h2>
-      <Link to="/contact" className="flat-button">CONTACT ME</Link>
-    </div>
+const Home = () => {
+  // Using the State Hook: https://reactjs.org/docs/hooks-state.html
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameArray = ['u', 'n', 'g'];
+  const jobArray = 'web developer'.split('');
 
-  </div>
-);
+  // https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          {/* add letterClass manually 12 means 1.2 seconds of delay */}
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>&#39;m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={18} />
+        </h1>
+        <h2>Frontend Developer</h2>
+        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
