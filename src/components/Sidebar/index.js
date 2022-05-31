dfdbc355188740ef37e664c109d6a234ae08b0f1
spero61 <spero61@gmail.com>
2022-05-31 15:45:50 +0900

    Add media query tag

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,13 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { /* faEnvelope, */ faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub, faSkype } from '@fortawesome/free-brands-svg-icons';
 import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
 
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
     </Link>
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
